7f37b8c20732b1c24341147b80037362e6f5d87c
AlexanderMar21 <54232340+AlexanderMar21@users.noreply.github.com>
2024-11-19 15:10:51 +0100

    feat: camera rework

--- src/lib/components/CameraFrame.tsx
+++ src/lib/components/CameraFrame.tsx
@@ -2,18 +2,21 @@
 
 import { useCallback, useEffect, useRef, useState, type FunctionComponent } from 'react';
 import Button from './Button/Button';
-import { createWorker, type ImageLike, type Word } from 'tesseract.js';
+import { createWorker, type ImageLike } from 'tesseract.js';
 
 interface CameraFrameProps {
 	onCloseClick?: () => void;
 }
 
 const CameraFrame: FunctionComponent<CameraFrameProps> = ({ onCloseClick }) => {
-	const videoRef = useRef<HTMLVideoElement>(null);
-	const canvasRef = useRef<HTMLCanvasElement>(null);
+	const [streaming, setStreaming] = useState(false);
 	const [imageWords, setImageWords] = useState<string[]>();
 
-	const [image, setImage] = useState<string | null>(null);
+	const videoRef = useRef<HTMLVideoElement>(null);
+	const canvasRef = useRef<HTMLCanvasElement>(null);
+
+	const width = 320;
+	const height = 240;
 
 	const facingMode = 'environment';
 	var constraints = {
@@ -23,17 +26,6 @@ const CameraFrame: FunctionComponent<CameraFrameProps> = ({ onCloseClick }) => {
 		},
 	};
 
-	const startCamera = useCallback(async () => {
-		try {
-			const stream = await navigator.mediaDevices.getUserMedia(constraints);
-			if (videoRef.current) {
-				videoRef.current.srcObject = stream;
-			}
-		} catch (err) {
-			console.error('Error accessing the camera', err);
-		}
-	}, []);
-
 	const recognizer = async (image: ImageLike) => {
 		const worker = await createWorker('eng');
 		const ret = await worker.recognize(image);
@@ -42,43 +34,35 @@ const CameraFrame: FunctionComponent<CameraFrameProps> = ({ onCloseClick }) => {
 		await worker.terminate();
 	};
 
-	const capturePhoto = useCallback(async () => {
-		if (videoRef.current && canvasRef.current) {
-			const context = canvasRef.current.getContext('2d');
-			if (context) {
-				// Get the video dimensions
-				const videoWidth = videoRef.current.videoWidth;
-				const videoHeight = videoRef.current.videoHeight;
-
-				// Calculate the dimensions and position of the frame to capture
-				const frameSizeX = 160;
-				const frameSizeY = 60;
-				const startX = (videoWidth - frameSizeX) / 2;
-				const startY = (videoHeight - frameSizeY) / 2;
-
-				// Set canvas size to match the frame size
-				canvasRef.current.width = frameSizeX;
-				canvasRef.current.height = frameSizeY;
-
-				// Draw the selected portion of the video onto the canvas
-				context.drawImage(
-					videoRef.current,
-					startX,
-					startY,
-					frameSizeX,
-					frameSizeY, // Source rectangle
-					0,
-					0,
-					frameSizeX,
-					frameSizeY // Destination rectangle
-				);
-
-				const imageDataUrl = canvasRef.current.toDataURL('image/png');
-				setImage(imageDataUrl);
-				await recognizer(imageDataUrl);
+	const startCamera = useCallback(async () => {
+		try {
+			const stream = await navigator.mediaDevices.getUserMedia(constraints);
+			if (videoRef.current) {
+				videoRef.current.srcObject = stream;
+				videoRef.current.play();
 			}
+		} catch (err) {
+			console.error(`An error occurred: ${err}`);
 		}
-		stopCamera();
+	}, []);
+
+	const clearPhoto = useCallback(() => {
+		const context = canvasRef.current?.getContext('2d');
+		if (context) {
+			context.fillStyle = '#AAA';
+			context.fillRect(0, 0, width, height);
+		}
+	}, []);
+
+	const takePicture = useCallback(async () => {
+		const context = canvasRef.current?.getContext('2d');
+		if (context && videoRef.current && canvasRef.current) {
+			context.drawImage(videoRef.current, 0, 0, width, height);
+
+			const data = canvasRef.current?.toDataURL('image/png');
+			await recognizer(data);
+		}
+		clearPhoto();
 	}, []);
 
 	const stopCamera = () => {
@@ -88,12 +72,21 @@ const CameraFrame: FunctionComponent<CameraFrameProps> = ({ onCloseClick }) => {
 		}
 	};
 
-	const resetCapture = useCallback(async () => {
-		setImage(null);
-		setImageWords([]);
-		if (videoRef.current && videoRef.current.srcObject) {
-			await startCamera();
-		}
+	useEffect(() => {
+		startCamera();
+		const handleCanPlay = () => {
+			if (!streaming) {
+				setStreaming(true);
+			}
+		};
+
+		const videoElement = videoRef.current;
+		videoElement?.addEventListener('canplay', handleCanPlay);
+
+		return () => {
+			videoElement?.removeEventListener('canplay', handleCanPlay);
+			stopCamera();
+		};
 	}, []);
 
 	const onCloseBtnClick = () => {
@@ -101,49 +94,32 @@ const CameraFrame: FunctionComponent<CameraFrameProps> = ({ onCloseClick }) => {
 		onCloseClick?.();
 	};
 
-	useEffect(() => {
-		startCamera();
-
-		return stopCamera;
-	});
-
 	return (
-		<div className="bg-gray-700/50 backdrop-blur fixed inset-0 z-10 flex items-center flex-col gap-6 justify-center">
-			<button onClick={onCloseBtnClick}>X</button>
-			{!image ? (
-				<div className="relative w-[320px] aspect-video bg-gray-200 rounded-lg overflow-hidden">
-					<video
-						ref={videoRef}
-						autoPlay
-						playsInline
-						className="absolute inset-0 w-full h-full object-cover rounded-xl"
-						aria-label="Camera feed"
-					/>
-					<div className="absolute border-solid border-2 border-indigo-600 h-1/3 w-1/2 transform translate-x-[-50%] translate-y-[-50%] top-[50%] left-[50%]"></div>
-				</div>
-			) : (
-				<div className="relative  w-[320px] aspect-video bg-gray-200 rounded-lg overflow-hidden">
-					<img
-						src={image}
-						alt="Captured photo"
-						className="absolute inset-0 w-full h-full object-cover rounded-xl"
-					/>
-				</div>
-			)}
-			<canvas ref={canvasRef} className="hidden" width="640" height="480" />
+		<div className="bg-gray-700/50 backdrop-blur fixed inset-0 z-10 flex items-center flex-col gap-6 justify-center px-4">
+			<div>
+				<video
+					ref={videoRef}
+					style={{ width: '100%', height: 'auto', backgroundColor: '#ddd', borderRadius: '4px' }}
+					width={width}
+					height={height}
+				>
+					Your browser does not support the video tag.
+				</video>
+			</div>
+			<canvas ref={canvasRef} style={{ display: 'none' }} width={width} height={height} />
 			<div className="flex items-center justify-center flex-wrap gap-4">
 				{imageWords?.map((word, index) => (
-					<span className="inline-block py-1 px-2 rounded-full bg-indigo-800 text-white" key={word + index}>
+					<span className="inline-flex py-1 px-3 rounded-full bg-indigo-800 text-white" key={word + index}>
 						{word}
 					</span>
 				))}
 			</div>
-			<div className="flex items-center justify-stretch gap-4">
-				<Button theme="success" onClick={capturePhoto}>
-					Take Photo
+			<div className="flex flex-col items-center gap-3">
+				<Button onClick={takePicture} theme="success">
+					Scan for numbers
 				</Button>
-				<Button theme="secondary" onClick={resetCapture}>
-					Retake
+				<Button onClick={onCloseBtnClick} theme="secondary">
+					Close
 				</Button>
 			</div>
 		</div>
